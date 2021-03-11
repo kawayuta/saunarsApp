83046140f8f5177d6a8f5b3c30a2ab8456e5b27a
kawayuta <addkawayuta@gmail.com>
2021-03-11 13:46:53 +0900

    Initial Commit

---
+++ Shared/saucialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  saucialAppApp.swift
+//  Shared
+//
+//  Created by kawayuta on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct saucialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
